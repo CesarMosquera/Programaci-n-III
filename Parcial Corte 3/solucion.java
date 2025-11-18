b14b08a4828f719123f690ee41cadcde6fe6c55d
CesarMosquera <cesarmosquera389@gmail.com>
2025-11-18 21:44:06 +0000

    Trabajo Final Corte 3

--- Parcial Corte 3/solucion.java
+++ Parcial Corte 3/solucion.java
@@ -1,61 +1,103 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+
 public class solucion {
+
     
+    static String[] ciudades = { "Cali", "Buga", "Medellin", "Monteria", "Cartagena" };
+
     public static void main(String[] args) {
-        
-        int[][] grafo = {
-            {0, 2, 0, 0},
-            {2, 0, 1, 4},
-            {0, 1, 0, 2},
-            {0, 4, 2, 0}
-        };
-    int n = grafo.length;
-    boolean[] verificado= new boolean[n];
-    int []distancias=new int[n]; 
-    int comienzo= 0;
 
-    for (int i = 0; i < n; i++) distancias[i] = Integer.MAX_VALUE;
-        distancias[comienzo] = 0;
-     for (int i = 0; i < n - 1; i++) {
-            int u = -1;
-    for (int j = 0; j < n; j++) {
-    if (!verificado[j] && (u == -1 || distancias[j] < distancias[u])) {
-    u = j;
 
+        ArrayList<ArrayList<int[]>> grafo = new ArrayList<>();
+
+        for (int i = 0; i < ciudades.length; i++) {
+            grafo.add(new ArrayList<>()); 
         }
-      }
-      verificado[u]=true;
 
-      for (int v = 0; v < n; v++) {
-                if (!verificado[v] && grafo[u][v] != 0 &&
-                    distancias[u] != Integer.MAX_VALUE &&
-                    distancias[u] + grafo[u][v] < distancias[v]) {
-                    distancias[v] = distancias[u] + grafo[u][v];
-                }
+
+        // Cali - Buga - Medellín - Montería - Cartagena
+        agregarArista(grafo, 0, 1, 67);   // Cali - Buga
+        agregarArista(grafo, 1, 2, 240);  // Buga - Medellin
+        agregarArista(grafo, 2, 3, 400);  // Medellin - Montería
+        agregarArista(grafo, 3, 4, 180);  // Montería - Cartagena
+
+     
+        int inicio = 0;  // Cali
+        int fin = 4;     // Cartagena
+
+        int[] distancias = dijkstra(grafo, inicio);
+
+        System.out.println("Distancia más corta de " + ciudades[inicio] +
+                " a " + ciudades[fin] + ": " + distancias[fin] + " km");
+
+       
+        int[] resultados = Arrays.copyOf(distancias, distancias.length);
+        burbuja(resultados);
+
+        System.out.println("\nDistancias ordenadas (burbuja):");
+        for (int d : resultados) {
+            System.out.println(d + " km");
+        }
     }
-    System.out.println("Distancias"+":"+comienzo);
-    for (int j=0;j<n; j++){
-      System.out.println("El nodo"+i+"="+distancias[i]);
+
+    
+    static void agregarArista(ArrayList<ArrayList<int[]>> grafo, int origen, int destino, int distancia) {
+        grafo.get(origen).add(new int[]{ destino, distancia }); 
+        grafo.get(destino).add(new int[]{ origen, distancia }); 
     }
-  }  
-  //Burbija
- 
-    public static void burbuja(int[] arr) {
-        int n = arr.length;
+
+    
+    static int[] dijkstra(ArrayList<ArrayList<int[]>> grafo, int inicio) {
+
+        int n = grafo.size();
+        int[] dist = new int[n];
+        boolean[] visitado = new boolean[n];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[inicio] = 0;
+
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
 
+            
+            int u = -1;
+            for (int j = 0; j < n; j++) {
+                if (!visitado[j] && (u == -1 || dist[j] < dist[u])) {
+                    u = j;
+                }
+            }
+
+            visitado[u] = true;
+
+        
+            for (int[] par : grafo.get(u)) {
+                int vecino = par[0];
+                int peso = par[1];
+
+                if (dist[u] + peso < dist[vecino]) {
+                    dist[vecino] = dist[u] + peso;
+                }
+            }
+        }
+
+        return dist;
     }
-    int[] burbuja = distancias.clone();
-        burbuja(burbuja);
-        System.out.println("Burbuja: " + Arrays.toString(burbuja));
-   }
-  }
-  }
+
+   
+    static void burbuja(int[] arr) {
+        boolean cambiado;
+        do {
+            cambiado = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    cambiado = true;
+                }
+            }
+        } while (cambiado);
+    }
 }
 
 
